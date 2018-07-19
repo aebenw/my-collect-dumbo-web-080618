9f2efdddba5ee613d9ff679fd314186b84a1d9bc
Andrew Woodward <ebenwoodward@icloud.com>
2018-07-19 23:29:14 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -12,3 +12,7 @@ my_collect(["Tim Jones", "Tom Smith", "Jim Campagno"]) do |name|
   name.split(" ").first
 end
 
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(collection) do |lang|
+  lang.upcase
+end
